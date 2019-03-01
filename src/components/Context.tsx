8894b7c7a43d3af90b0c5bd4d64c183a72a6917a
Dmitrij Podabed <d.podabed@belitsoft.com>
2019-03-01 22:38:58 +0300

    add initial context with hook

--- src/components/Context.tsx
+++ src/components/Context.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 
 interface IContextInitialState {
   isDark: boolean;
@@ -15,51 +16,10 @@ const Context: React.Context<IContextInitialState> = React.createContext<IContex
   initialState,
 );
 
-export class ContextProvider extends React.PureComponent<{}, IContextInitialState> {
-  constructor(props) {
-    super(props);
+export const ContextProvider = ({ children }) => {
+  const [isDark, toggleDark] = useState(false);
 
-    this.state = {
-      isDark: false,
-      toggleDark: this.toggleDark,
-    };
-  }
-
-  public componentDidMount() {
-    const isDarkFromLocalStorage: boolean = JSON.parse(localStorage.getItem('isDark'));
-
-    if (isDarkFromLocalStorage) {
-      this.setState({ isDark: isDarkFromLocalStorage });
-    } else if (this.isSupportsDarkModeInMacOS()) {
-      this.setState({ isDark: true });
-    }
-  }
-
-  public render() {
-    const { children } = this.props;
-    const { isDark } = this.state;
-
-    return (
-      <Context.Provider
-        value={{
-          isDark,
-          toggleDark: this.toggleDark,
-        }}
-      >
-        {children}
-      </Context.Provider>
-    );
-  }
-
-  public toggleDark: () => void = (): void => {
-    const isDark = !this.state.isDark;
-
-    localStorage.setItem('isDark', JSON.stringify(isDark));
-    this.setState({ isDark });
-  };
-
-  private isSupportsDarkModeInMacOS: () => boolean = (): boolean =>
-    window.matchMedia('(prefers-color-scheme: dark)').matches === true;
-}
+  return <Context.Provider value={{ isDark, toggleDark }}>{children}</Context.Provider>;
+};
 
 export default Context;
