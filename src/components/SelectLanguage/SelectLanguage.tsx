24cc436127d39918d9cdd5004b9cb5b72a8b5a88
Dmitrij Podabed <dmitrij@podabed.org>
2019-04-28 00:32:16 +0300

    simplify styles for settings

--- src/components/SelectLanguage/SelectLanguage.tsx
+++ src/components/SelectLanguage/SelectLanguage.tsx
@@ -1,24 +1,30 @@
-import { Link } from 'gatsby';
+import { navigate } from 'gatsby';
 import * as React from 'react';
-import { FormattedMessage } from 'react-intl';
 
-const SelectLanguage = (props) => {
-  const links = props.langsMenu.map((lang) => (
-    <li selected={lang.selected}>
-      <Link to={lang.link} onClick={() => props.toggleLanguage(props.locale)} key={lang.langKey}>
-        {lang.langKey}
-      </Link>
-    </li>
-  ));
+import { ILangObject } from '../../interfaces';
+import * as styles from './SelectLanguage.module.css';
 
-  return (
-    <section>
-      <header>
-        <FormattedMessage id='selectLanguage' />
-      </header>
-      <ul>{links}</ul>
-    </section>
+const SelectLanguage: React.FC = (props: any): React.ReactElement => {
+  const { langsMenu, toggleLanguage } = props;
+
+  const links: React.ReactElement[] = langsMenu.map(
+    (lang: ILangObject): React.ReactElement => {
+      const onClick: () => void = (): void => {
+        toggleLanguage(lang.langKey);
+        navigate(lang.link);
+      };
+      const styleLineThroughForUnselectedLanguage =
+        lang.selected === false ? styles.selectedLanguage : null;
+
+      return (
+        <button className={styles.languageChangeButton} key={lang.langKey} onClick={onClick}>
+          <span className={styleLineThroughForUnselectedLanguage}>{lang.langKey}</span>
+        </button>
+      );
+    },
   );
+
+  return <>{links}</>;
 };
 
 export default SelectLanguage;
