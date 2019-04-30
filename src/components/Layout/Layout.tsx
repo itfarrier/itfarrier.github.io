1689ab02d015869e8c94aa2c0f35f599808a815c
Dmitrij Podabed <dmitrij@podabed.org>
2019-04-30 09:14:42 +0300

    add dynamic meta info and icons

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -46,13 +46,86 @@ const Layout: React.FC = (props: any): React.ReactElement => {
   const homeLink: string = `/${langKey}/`;
   const langsMenu: ILangObject[] = getLangs(langs, langKey, getUrlForLang(homeLink, pathname));
   const i18nMessages: { [key: string]: string } = require(`../../data/messages/${langKey}`);
+  const generateAndroidChrome = (sizesInPx: number[]): React.ReactElement[] => {
+    return sizesInPx.map(
+      (size: number): React.ReactElement => (
+        <link
+          href={`/favicons/android-chrome-${size}x${size}.png`}
+          key={size}
+          rel={'icon'}
+          sizes={`${size}x${size}`}
+          type={'image/png'}
+        />
+      ),
+    );
+  };
+  const generateAppleTouch = (sizesInPx: number[]): React.ReactElement[][] => {
+    return sizesInPx.map(
+      (size: number): React.ReactElement[] => [
+        <link
+          href={`/favicons/apple-touch-icon-${size}x${size}.png`}
+          key={`1${size}`}
+          rel={'apple-touch-icon'}
+          sizes={`${size}x${size}`}
+          type={'image/png'}
+        />,
+        <link
+          href={`/favicons/apple-touch-icon-${size}x${size}-precomposed.png`}
+          key={`2${size}`}
+          rel={'apple-touch-icon'}
+          sizes={`${size}x${size}`}
+          type={'image/png'}
+        />,
+      ],
+    );
+  };
+  const generateFavicon = (sizesInPx: number[]): React.ReactElement[] => {
+    return sizesInPx.map(
+      (size: number): React.ReactElement => (
+        <link
+          href={`/favicons/favicon-${size}x${size}.png`}
+          key={size}
+          rel={'icon'}
+          sizes={`${size}x${size}`}
+          type={'image/png'}
+        />
+      ),
+    );
+  };
 
   return (
     <IntlProvider locale={langKey} messages={i18nMessages}>
       <Context.Consumer>
         {(context) => (
           <>
-            <Helmet title={title} />
+            <Helmet title={title}>
+              {generateAndroidChrome([36, 48, 72, 96, 144, 192, 256, 384, 512])}
+              <link
+                href={'/favicons/apple-touch-icon.png'}
+                rel={'apple-touch-icon'}
+                sizes={'180x180'}
+                type={'image/png'}
+              />
+              <link
+                href={'/favicons/apple-touch-icon-precomposed.png'}
+                rel={'apple-touch-icon'}
+                sizes={'180x180'}
+                type={'image/png'}
+              />
+              {generateAppleTouch([57, 60, 72, 76, 114, 120, 144, 152, 180])}
+              {generateFavicon([16, 32])}
+              <link color={'#000'} href={'/favicons/safari-pinned-tab.svg'} rel={'mask-icon'} />
+              <link href={'/favicons/favicon.ico'} rel={'shortcut icon'} />
+              <meta content={i18nMessages.description} name={'description'} />
+              <meta content={i18nMessages.keywords} name={'keywords'} />
+              <meta content={i18nMessages.title} name={'apple-mobile-web-app-title'} />
+              <meta content={i18nMessages.title} name={'application-name'} />
+              <meta content={'#fff'} name={'msapplication-TileColor'} />
+              <meta content={'/favicons/mstile-144x144.png'} name={'msapplication-TileImage'} />
+              <meta content={'/favicons/browserconfig.xml'} name={'msapplication-config'} />
+              <meta content={'#fff'} name={'theme-color'} />
+              <meta content={'black-translucent'} name={'apple-mobile-web-app-status-bar-style'} />
+            </Helmet>
             <Header
               context={context}
               homeLink={homeLink}
--- src/data/messages/en.js
+++ src/data/messages/en.js
@@ -1,21 +1,5 @@
 module.exports = {
-  title: 'Hugo Nogueira',
-  'header.title': 'Hugo Nogueira',
-  'header.subTitle': 'Engineering lead and agile manager.',
-  home: 'Home',
-  about: 'About',
-  blog: 'Blog',
-  all: 'All',
-  posts: 'Posts',
-  'posts.seeMore': 'See all posts →',
-  tags: 'Post Tags',
-  'tags.allTagsLink': 'Browse all tags',
-  'tags.nPostsTaggedWith': `{nPosts, number} {nPosts, plural,
-      one {post}
-      other {posts}
-    } tagged with`,
-  contact: 'Contact',
-  call: 'Schedule a call',
-  'index.featured': 'Featured',
-  'index.stories': 'All stories',
+  description: 'IT farrier',
+  keywords: [''],
+  title: 'podabed.org',
 };
