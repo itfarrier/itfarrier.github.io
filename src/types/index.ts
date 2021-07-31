2a6b23f8947f22eb3116c7e2e71a6734d6a6864f
Dmitrij Podabed <iam@itfarrier.com>
2021-07-31 13:44:54 +0300

    refactor types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,4 @@
-import { Link } from 'cmpts/HeaderLink/types';
 import { Language } from 'cmpts/LanguageContext';
-
-declare module '*.pdf' {
-  const pdf: Link;
-
-  export default pdf;
-}
 
 export interface ILangObject {
   langKey: Language;
