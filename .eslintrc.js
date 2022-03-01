69d036aee2d332c88af11d0131a1cee1d95786f0
Dmitrij Podabed <iam@itfarrier.com>
2022-03-01 22:02:52 +0300

    add linting with type information

--- .eslintrc.js
+++ .eslintrc.js
@@ -69,10 +69,12 @@ module.exports = {
       extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'plugin:prettier/recommended',
       ],
       files: ['{.,}*.ts{x,}'],
       parser: '@typescript-eslint/parser',
+      parserOptions: { project: ['./tsconfig.json'], tsconfigRootDir: __dirname },
       plugins: ['@typescript-eslint'],
     },
   ],
