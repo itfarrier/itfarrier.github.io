195e80605d1e7c2e5b835c74b018230efe8e5f2f
Dmitrij Podabed <iam@itfarrier.com>
2022-03-01 21:22:13 +0300

    add sort rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,45 @@
 module.exports = {
   overrides: [
     {
-      env: { amd: true, commonjs: true, node: true },
+      env: { amd: true, commonjs: true, es2022: true, node: true },
       extends: ['eslint:recommended', 'plugin:prettier/recommended'],
       files: ['**/*.config.js', '**/{,.}*rc*.js'],
+      plugins: ['import', 'sort-keys-fix', 'sort-destructure-keys'],
+      rules: {
+        'import/order': [
+          'error',
+          {
+            alphabetize: { order: 'asc' },
+            groups: [
+              'builtin',
+              'external',
+              'internal',
+              'parent',
+              'sibling',
+              'index',
+              'object',
+              'unknown',
+            ],
+            'newlines-between': 'always',
+            pathGroups: [{ group: 'builtin', pattern: 'react', position: 'before' }],
+            pathGroupsExcludedImportTypes: ['react'],
+          },
+        ],
+        'sort-destructure-keys/sort-destructure-keys': 2,
+        'sort-imports': [
+          'error',
+          {
+            allowSeparatedGroups: true,
+            ignoreCase: false,
+            ignoreDeclarationSort: false,
+            ignoreMemberSort: false,
+            memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+          },
+        ],
+        'sort-keys': ['error', 'asc', { caseSensitive: true, minKeys: 2, natural: false }],
+        'sort-keys-fix/sort-keys-fix': 'error',
+        'sort-vars': 'error',
+      },
     },
     {
       extends: [
