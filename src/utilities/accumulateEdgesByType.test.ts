0f042c7c0f5cf3ff1722368c423ea12a89ed1655
Dmitrij Podabed <iam@itfarrier.com>
2022-04-06 19:36:57 +0300

    add second part of test

--- src/utilities/accumulateEdgesByType.test.ts
+++ src/utilities/accumulateEdgesByType.test.ts
@@ -5,7 +5,7 @@ import { accumulateEdgesByType } from 'src/utilities/accumulateEdgesByType';
 type MarkdownRemarkEdge = GatsbyTypes.MarkdownRemarkEdge;
 
 describe('accumulateEdgesByType', () => {
-  it('returns initial structure with empty array', () => {
+  it('returns initial structure with example object inside the right lang key', () => {
     expect(
       accumulateEdgesByType(
         GROUPED_BY_TYPE_AND_LANGUAGE_FALLBACK,
@@ -15,6 +15,18 @@ describe('accumulateEdgesByType', () => {
       ),
     ).toEqual({
       [FRONTMATTER_TYPES.PAGE]: { [LANGUAGE_CODES.EN]: [{ isExample: true }] },
+      [FRONTMATTER_TYPES.POST]: {},
+    });
+
+    expect(
+      accumulateEdgesByType(
+        GROUPED_BY_TYPE_AND_LANGUAGE_FALLBACK,
+        { isExample: true } as unknown as MarkdownRemarkEdge,
+        LANGUAGE_CODES.RU,
+        FRONTMATTER_TYPES.PAGE,
+      ),
+    ).toEqual({
+      [FRONTMATTER_TYPES.PAGE]: { [LANGUAGE_CODES.RU]: [{ isExample: true }] },
       [FRONTMATTER_TYPES.POST]: {},
     });
   });
