4295103b45b743dfc27a4c6e7a5fd98cbcc35060
Dmitrij Podabed <iam@itfarrier.com>
2022-04-08 00:17:09 +0300

    add MARKDOWN_EDGE_FALLBACK

--- src/constants/fallbacks.tsx
+++ src/constants/fallbacks.tsx
@@ -1,5 +1,14 @@
+import { Edge } from 'src/types';
+
 export const EMPTY_ARRAY = [];
 export const EMPTY_COMPONENT = () => <></>;
 export const EMPTY_FUNCTION = () => {};
 export const EMPTY_OBJECT = {};
 export const EMPTY_STRING = '';
+
+export const MARKDOWN_EDGE_FALLBACK: Edge = {
+  node: {
+    fields: { langKey: EMPTY_STRING, slug: EMPTY_STRING },
+    frontmatter: { language: EMPTY_STRING, title: EMPTY_STRING, type: EMPTY_STRING },
+  },
+};
