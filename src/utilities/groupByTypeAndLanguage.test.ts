1536f183ed6b81636587bcb85f0cdff52cf6b3f4
Dmitrij Podabed <iam@itfarrier.com>
2022-04-07 21:52:17 +0300

    fix test

--- src/utilities/groupByTypeAndLanguage.test.ts
+++ src/utilities/groupByTypeAndLanguage.test.ts
@@ -1,32 +1,8 @@
-import { LANGUAGE_CODES } from 'src/constants';
+import { EMPTY_OBJECT } from 'src/constants/fallbacks';
 import { groupByTypeAndLanguage } from 'src/utilities/groupByTypeAndLanguage';
-
-type MarkdownRemarkEdge = GatsbyTypes.MarkdownRemarkEdge;
 
 describe('groupByTypeAndLanguage', () => {
   it('returns initial structure with example object inside the right lang key', () => {
-    expect(
-      groupByTypeAndLanguage(
-        GROUPED_BY_TYPE_AND_LANGUAGE_FALLBACK,
-        { isExample: true } as unknown as MarkdownRemarkEdge,
-        LANGUAGE_CODES.EN,
-        FRONTMATTER_TYPES.PAGE,
-      ),
-    ).toEqual({
-      [FRONTMATTER_TYPES.PAGE]: { [LANGUAGE_CODES.EN]: [{ isExample: true }] },
-      [FRONTMATTER_TYPES.POST]: {},
-    });
-
-    expect(
-      groupByTypeAndLanguage(
-        GROUPED_BY_TYPE_AND_LANGUAGE_FALLBACK,
-        { isExample: true } as unknown as MarkdownRemarkEdge,
-        LANGUAGE_CODES.RU,
-        FRONTMATTER_TYPES.PAGE,
-      ),
-    ).toEqual({
-      [FRONTMATTER_TYPES.PAGE]: { [LANGUAGE_CODES.RU]: [{ isExample: true }] },
-      [FRONTMATTER_TYPES.POST]: {},
-    });
+    expect(groupByTypeAndLanguage(EMPTY_OBJECT, { isExample: true })).toEqual(EMPTY_OBJECT);
   });
 });
