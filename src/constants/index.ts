e01f53f9563dbdb4746446e60c659963aabd730e
Dmitrij Podabed <iam@itfarrier.com>
2022-04-07 19:55:55 +0300

    remove FRONTMATTER_TYPES

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,8 +11,3 @@ export enum COLORS {
   RGBA_0_0_0_100 = 'rgba(0, 0, 0, 1)',
   RGBA_255_255_255_100 = 'rgba(255, 255, 255, 1)',
 }
-
-export enum FRONTMATTER_TYPES {
-  PAGE = 'pages',
-  POST = 'posts',
-}
