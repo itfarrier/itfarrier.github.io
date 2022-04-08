c28ce5e9212e7dc502391734449dbb7a9e53a632
Dmitrij Podabed <iam@itfarrier.com>
2022-04-08 20:00:32 +0300

    fix Head query type

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -5,6 +5,7 @@ import { graphql, useStaticQuery } from 'gatsby';
 
 import { HeadView } from 'cmpts/Head/HeadView';
 import { LanguageContext } from 'cmpts/LanguageContext';
+import { HeadQuery } from 'root/graphql-types';
 import { siteMetadata } from 'src/data/siteMetadata';
 import { generateAndroidChromeHeadLinks } from 'src/utilities/generateAndroidChromeHeadLinks';
 import { generateAppleTouchHeadLinks } from 'src/utilities/generateAppleTouchHeadLinks';
@@ -23,7 +24,7 @@ export const Head: FC = () => {
 
   const { href } = useLocation();
 
-  const data = useStaticQuery<GatsbyTypes.HeadQuery>(graphql`
+  const data = useStaticQuery<HeadQuery>(graphql`
     query Head {
       site {
         siteMetadata {
