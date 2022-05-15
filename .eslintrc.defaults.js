11e68cb9e7a57dcaf07e6f3b1e409a1ced7c98b9
Dmitrij Podabed <iam@itfarrier.com>
2022-05-15 14:52:56 +0300

    remove old linter configs

--- .eslintrc.defaults.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  env: { amd: true, commonjs: true, es2021: true, jest: true, node: true },
-  parserOptions: { ecmaFeatures: { impliedStrict: true, jsx: true }, sourceType: 'module' },
-  root: true,
-};
