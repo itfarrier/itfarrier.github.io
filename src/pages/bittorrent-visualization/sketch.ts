b4a010fd8de0541cb31a67d3ffb76a172ec5904e
Dmitry Podabed <itfarrier@icloud.com>
2023-11-18 12:52:33 +0300

    add sketch

---
+++ src/pages/bittorrent-visualization/sketch.ts
@@ -0,0 +1,42 @@
+import type p5 from 'p5';
+
+export const sketch = (p5: p5) => {
+  class Bit {
+    bitHue: number;
+    id: number;
+
+    constructor(i: number, hu: number) {
+      this.id = i;
+      this.bitHue = hu;
+    }
+  }
+
+  class Torrent {
+    bits: Bit[] = [];
+
+    constructor(totbits: number) {
+      for (let i = 0; i < totbits; i++) {
+        const ll = (255 / totbits) * i;
+        const k = new Bit(i, ll);
+
+        this.bits.push(k);
+      }
+    }
+  }
+
+  const angle = 0;
+  const peers = [];
+  const tmp = [];
+  const connections = [];
+  const rot = -1;
+  const testTorrent = new Torrent(30);
+  const initialSeeders = 1;
+  const initialPeers = 5;
+
+  p5.setup = () => {
+    p5.createCanvas(450, 450, p5.WEBGL);
+    p5.fill(256);
+    p5.fill(0);
+    p5.textAlign(p5.CENTER);
+  };
+};
